7d03ee34493983472b0062ca153dd0f30d34d7ee
Hoang Anh Huy Luu <61114077+luuhoanganhhuy@users.noreply.github.com>
2020-08-19 12:15:31 +0700

    add-character

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,7 +9,7 @@ public class Movement : MonoBehaviour{
 
 
     // Update is called once per frame
-    /*
+    
     void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.UpArrow))
@@ -30,12 +30,12 @@ public class Movement : MonoBehaviour{
             rb.AddForce(Vector3.left * speed* Time.deltaTime);
         }
     }
-    */
-
+    
+    /*
     void FixedUpdate()
     {
         var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         transform.position += move * speed * Time.deltaTime;
     }
-
+    */
 }
