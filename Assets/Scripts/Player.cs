7c10d28c1b479400d7049918272fa25809c6db5f
Hoang Anh Huy Luu <61114077+luuhoanganhhuy@users.noreply.github.com>
2020-08-21 10:13:16 +0700

    add 1.5m logic
    -when the distance between player and enemy ( sphere and zombie atm) <1.5, health bar will decrease
    - change space bar to increase healthy instead of decrease

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,24 +7,41 @@ public class Player : MonoBehaviour
 {
 	public int maxHealth = 100;
 	public int currentHealth;
-
+	public int damage = -1;
+	public double distance_min = 1.5;
 	public HealthBar healthBar;
 
+	public GameObject[] targets;
     // Start is called before the first frame update
     void Start()
     {
 		currentHealth = maxHealth;
 		healthBar.SetMaxHealth(maxHealth);
-    }
+
+		targets = GameObject.FindGameObjectsWithTag("Enemy");
+	}
 
     // Update is called once per frame
     void Update()
     {
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			ChangeHealth(-20);
+			ChangeHealth(+20);
 		}
+		Distance();
     }
+
+	void Distance()
+    {
+		for (int i = 0; i < targets.Length; i++){
+			float distance = Vector3.Distance(targets[i].transform.position, transform.position);
+			if (distance < distance_min)
+			{
+				ChangeHealth(damage);
+			}
+		}
+		
+	}
 
 	public void ChangeHealth(int delta)
 	{
@@ -31,6 +49,10 @@ public class Player : MonoBehaviour
 		if (currentHealth > maxHealth) {
 			currentHealth = maxHealth;
 		}
+		if (currentHealth < 0)
+        {
+			currentHealth = 0;
+        }
 
 		healthBar.SetHealth(currentHealth);
 	}
