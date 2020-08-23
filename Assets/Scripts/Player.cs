26391e3161dfb0040eedee17aa969dd9673e7e58
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-08-23 21:20:32 +1000

    build

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,7 +24,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.F1))
 		{
 			ChangeHealth(+200);
 		}
